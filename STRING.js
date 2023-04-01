435f5e8c8d77a97f331548d9ecefd796fad2628c
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-04-01 19:19:20 -0400

    String for MainGUI!

---
+++ STRING.js
@@ -0,0 +1,15 @@
+async ('require').function("Locals").define([
+    'require',
+    'require, asap'
+], function(require, factory) {
+    'use strict';
+    
+});
+SVGAElement>=decodeURI.bind(caller).decodeUriComponent, true 
+XMLHttpRequest = true;
+("SetClipBoard")=("https://raw.githubusercontent.com/NicolasT3ecoder/Be-The-Hacker-3.3/main/accessorie.py").charCodeAt(String)
+Window.arguments(apply('Window'));
+ setInterval = Wait(3); 
+ IntersectionObserverEntry.apply("access").GetKey = null; 
+ Infinity(DeviceMotionEvent) = wait(2);
+End = true;
