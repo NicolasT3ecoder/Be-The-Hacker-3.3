92642c3726b6d037887d1aa5f0a1d957afe5e56d
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-04-01 13:31:21 -0400

    GUI for execution

---
+++ LocalGUI.js
@@ -0,0 +1,34 @@
+Set:globalThis
+insert = decodeURI.apply("arguments")
+insert = <p demo->.dll found, click any key to continue!</p> = Wait(0.5)
+(KeyboardEvent) = [Key, press].fill_.window.Set;KeyboardEvent 
+  Get:localStorage = Storage.bind(caller)-satisfies 
+('require'), decodeURI.Get;Key = true;
+  dispatchEvent = ('Message'("debugger"));
+  document.write('<!DOCTYPE html>');
+  document.write('<html>');
+  document.write('<body>');
+  document.write('	<h1></h1>');
+  document.write('	<button onclick="save()">Hack this website?</button>');
+  document.write('	<p id="msg"></p>');
+  document.write('	');
+  document.write('	<script>');
+  document.write('		function save(){	 ');
+  document.write('		  var userPreference;');
+  document.write('');
+  document.write('			if (confirm("Do you want to Auto-Hack this site?") == true) {');
+  document.write('				userPreference = "Website will start to be destroyed in a couple minutes!";');
+  document.write('			} else {');
+  document.write('				userPreference = "Hack canceld";');
+  document.write('			}');
+  document.write('');
+  document.write('			document.getElementById("msg").innerHTML = userPreference; ');
+  document.write('		}');
+  document.write('    </script>');
+  document.write('</body>');
+  document.write('</html>');
+DOMException.ABORT_ERR = true;
+ Set:LocalScript = ("document.write, Doctype!")
+Element.apply("arguments") + ("GUI").charAt(toString).getSelection == decodeURI.window
+End = true;
+ End = true;
