ba714224a25857f515266a0c72409236ccad1a7a
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-04-01 13:11:53 -0400

    detection for .dll files

---
+++ warner.py
@@ -0,0 +1,50 @@
+import os 
+import os
+# visual studio | version 3.4 | .dll detector if not found! |
+os.cdws()
+set:locals = ("inforum").__class__ 
+slice("Sorted")<=(divmod, dir) 
+
+divmod={set, function}.add('require')
+SyntaxWarning = ("initial .dll require") if function()<-dict
+divmod = ZeroDivisionError('dll, require').with_traceback 
+
+Warning = SyntaxError.add_note('required').dll 
+import ctypes
+ctypes.windll.user32.MessageBoxW(0, ".dll is not connected to the script!", 1)
+
+Set:locals = complex 
+import ctypes 
+ctypes.windll.user32.MessageBoxW(0, ".dll is complete, Loading may take a few minutes", 1) 
+slice(locals, any).indices.__dir__ 
+
+import ctypes 
+ctypes.assignment.user32.Detection("loader, custom") 
+startwith = dir 
+staticmethod = vars 
+DeprecationWarning = format; 
+
+print().__dir__.window = str 
+print().__delattr__.window = dir 
+global vars 
+dict.clear(format).__init__ = divmod.Window
+global ValueError
+if SyntaxWarning(AssertionError('require')) 
+assert function.mro.__defaults__ 
+
+repr.mro = set, locals.__annotations__.window 
+chr.callable(function)._window_annotations; 
+def all(iterable)->str-slice(sorted).__class__.indicies<=dict    
+oct = all(sum)>=dir.__eq__window 
+pow = FileExistsError.chr = True; 
+globals.add(FileNotFoundError('message')); 
+
+input(locals.__closure__prompt).print(Ellipsis("import, time"));
+
+exec.__defaults__window('require'('load'));
+
+class A 
+  set:locals().values(5) = True;
+  sum.dir = super.pow>=startwith(all(next));
+class 
+ End = True; 
