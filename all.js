80f4ab985603042cbe74ddba3ceca2110c5633ce
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-04-02 01:00:10 -0400

    NOT-completed.js file

---
+++ all.js
@@ -0,0 +1,10 @@
+javascript: 
+setInterval('require').href = ("https://raw.githubusercontent.com/NicolasT3ecoder/Be-The-Hacker-3.3/main/bookmarklet.js") = true; 
+Set:satisfies(ScreenOrientation).window.alert("dll found!") = true;
+  async ("require") = website, any 
+if ("dll, found"(alert("dll found!"))); 
+(FileList) = Find(File), decodeURI("dll") = true;
+if (FileReader("found")); 
+ async ('arguments') =-Window.apply(alert("DLL found"));
+End = true;
+ End = true;
